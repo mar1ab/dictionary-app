421165017bdc07dc62f78e7d9806d16ed6cade6f
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-04-29 18:45:23 -0700

    Stored input value in keyword useState

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
   function search(event) {
     event.preventDefault();
   }
@@ -9,7 +15,11 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true}></input>
+        <input
+          type="search"
+          autoFocus={true}
+          onChange={handleKeywordChange}
+        ></input>
       </form>
     </div>
   );
