fa5a23cdd1ecc6c646005153761c12f26c605bcb
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-04-30 13:40:08 -0700

    Integrated dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,6 +11,12 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleResponse(response) {
+    return <div></div>;
   }
 
   return (
