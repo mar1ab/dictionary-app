5721711b11bbac767c8098494f9f6b724956ce61
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-04-29 18:13:12 -0700

    Set up Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Dictionary />
+    </div>
+  );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Dictionary() {
+  return <div></div>;
+}
