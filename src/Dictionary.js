2936ea686e15660f7609870b001bec835b31bfaa
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-04-29 18:38:22 -0700

    Created search function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  function search(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="Dictionary">
-      <form>
+      <form onSubmit={search}>
         <input type="search" autoFocus={true}></input>
       </form>
     </div>
