c76b5ce4348fddaa9360966374f62ed82b9bec89
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-05-05 15:24:15 -0700

    Added footer with GitHub link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ export default function App() {
   return (
     <div className="App container">
       <Dictionary />
+      <p className="footer">
+        Built by Maria Bodwell. View on{" "}
+        <a
+          href="https://github.com/mar1ab/dictionary-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
     </div>
   );
 }
