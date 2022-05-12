755a68d43f4583ddd811abb4ca086bb0cbbb3030
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-05-11 20:18:51 -0700

    Styling on Synonym section

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <div>
+      <div className="Synonyms">
         Synonyms:
-        <ul className="Synonyms">
+        <ul>
           {props.synonyms.map(function (synonym, index) {
             return <li key={index}>{synonym}</li>;
           })}
