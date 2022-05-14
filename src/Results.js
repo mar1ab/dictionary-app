4c5b84131ff312c23d99ca5f36c1a2f84e7e0f27
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-05-14 13:17:17 -0700

    Created Phonetic component

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h1 className="text-capitalize">{props.results.word}</h1>
+        {props.results.phonetics.msp(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
