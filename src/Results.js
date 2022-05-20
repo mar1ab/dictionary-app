4612e8e4dcaebcbb53507c570ec4a59d106f46fe
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-05-20 13:58:54 -0700

    Undid automatic text-capitalize for keyword

--- src/Results.js
+++ src/Results.js
@@ -8,7 +8,7 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h1 className="text-capitalize">{props.results.word}</h1>
+        <h1>{props.results.word}</h1>
         {props.results.phonetics.map(function (phonetic, index) {
           return (
             <div key={index}>
