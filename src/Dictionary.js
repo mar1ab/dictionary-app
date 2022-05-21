78a5d96edcdc28f11d7b382570db77773eaad67d
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-05-21 12:59:22 -0700

    Updated API call to display 9 images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -24,7 +24,7 @@ export default function Dictionary(props) {
 
     let pexelsApiKey =
       "563492ad6f91700001000001f705712e020244aa816cfb85fc2d9eb2";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
     let headers = { Authorization: `${pexelsApiKey}` };
     axios
       .get(pexelsApiUrl, {
