43e17edddd46a590b4649cd40dc59acbf9fc0041
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-05-21 12:52:45 -0700

    Photos component now generates images

--- src/Photos.js
+++ src/Photos.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function Photos(props) {
-  return <div></div>;
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <img src={photo.src.landscape} className="img-fluid" />
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
 }
